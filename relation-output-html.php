558262b6349f808af13615f46464d6c75ff09829
Claudioweb <contato@claudioweb.com.br>
2021-02-23 16:05:33 -0300

    version 2

--- relation-output-html.php
+++ relation-output-html.php
@@ -3,7 +3,7 @@
 Plugin Name:  Relation Output HTML
 Plugin URI:   http://www.claudioweb.com.br/
 Description:  Este plugin transforma todos os conteúdos salvos e relaxionados em páginas staticas HTML para servidores como FTP e S3
-Version:      1.0
+Version:      2.0
 Author:       Claudio Rabelo (claudioweb)
 Author URI:   http://www.claudioweb.com.br/
 Text Domain:  relation-output-html
